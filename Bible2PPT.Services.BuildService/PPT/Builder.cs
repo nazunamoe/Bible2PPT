7f9b412f861ec54a9a34f4a73c4377ee85b22406
nazunamoe <nakashimayuki@kakao.com>
2025-06-21 22:38:26 +0900

    PPT 장수 조절 부분 수정

--- Bible2PPT.Services.BuildService/PPT/Builder.cs
+++ Bible2PPT.Services.BuildService/PPT/Builder.cs
@@ -125,7 +125,7 @@ public class Builder : JobManager
             {
                 _ = await Task.WhenAny(channel.Reader.WaitToReadAsync(token).AsTask(), produce).ConfigureAwait(false);
 
-                if (!channel.Reader.TryRead(out var item) || chaptersDoneCount == 1)
+                if (!channel.Reader.TryRead(out var item) || chaptersDoneCount == chaptersCount)
                 {
                     break;
                 }
